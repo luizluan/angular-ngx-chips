a55589b756d1438b72a2072abb2b364737d5d67f
Luiz Luan <luan@easyteam.rocks>
2023-09-19 14:31:03 -0300

    'deploy'

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ call npm run build
 cd dist
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/luizluan/angular-ngx-chips.git master
+git push -f https://github.com/luizluan/angular-ngx-chips.git npm
 cd ..
 
