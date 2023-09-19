a164df9a5d4c666bf4a41c05cadd82a6da04eb39
Luiz Luan <luan@easyteam.rocks>
2023-09-19 14:58:00 -0300

    Update

--- build.bat
+++ build.bat
@@ -1,13 +1,34 @@
+@echo off
+
+rem Remove the 'dist' directory (if it exists)
 rmdir /S /Q dist
 
+rem Install npm dependencies
 call npm i
+
+rem Run the build command
 call npm run build
 
+rem Enter the 'dist' directory
 cd dist
-rmdir /S /Q ".git"
-git init
-git add -A
-git commit -m 'deploy'
-git push -f https://github.com/luizluan/angular-ngx-chips.git npm
-cd ..
 
+rem Delete the '.git' folder in the 'dist' directory (if it exists)
+rmdir /S /Q ".git"
+
+rem Initialize a Git repository in the 'dist' directory
+git init
+
+rem Add all files and folders to the Git repository
+git add -A
+
+rem Commit the changes
+git commit -m "deploy"
+
+rem Configure the "origin" remote repository with the GitHub URL
+git remote add origin https://github.com/luizluan/angular-ngx-chips.git
+
+rem Push to the "npm" branch in the "origin" remote repository
+git push -f origin npm
+
+rem Navigate back to the previous directory
+cd ..
